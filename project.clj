18a0dec132fd9d9a11e83f3a4ef7c4e6b4916011
Petr Yanovich <fl00r@yandex.ru>
2016-10-28 12:19:04 +0300

    profile

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tarantool-clj "0.1.0"
-  :description "FIXME: write description"
+  :description "Clojure Tarantool Connector"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
