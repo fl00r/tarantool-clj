89f0c3e196152f5153c4b26c6a9b4599142ec166
Sergey Bronnikov <sergeyb@tarantool.org>
2020-07-29 11:21:04 +0300

    remove useless test in core

--- test/tarantool_clj/core_test.clj
+++ test/tarantool_clj/core_test.clj
@@ -1,7 +1,3 @@
 (ns tarantool-clj.core-test
   (:require [clojure.test :refer :all]
             [tarantool-clj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
